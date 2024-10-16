dd0cb0edcf712a66afa9ff64cbb39de4e31e4931
Anil Kumar Behera <ark845612@gmail.com>
2024-10-16 22:47:35 +0530

    test

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,20 @@
 import express from "express"
-import path from "path"
 import { handler } from "./controller";
 
 export const app = express();
 
 app.use(express.json());
-app.use(express.static(path.join(__dirname, 'public')));
+
 
 app.use((req, res, next) => {
     console.log(req.method, req.path);
     next();
 })
 
-// const PORT = config.port;
-
-// app.get("/xyz", (req, res) => {
-//     console.log("xyz")
-//     res.json({
-//         message: "hellllodasj"
-//     })
-// })
-app.get("/home", async (req, res) => {
-    res.sendFile(path.join(__dirname, "public", 'index.html'))
+app.get("/", async (req, res) => {
+    res.json({
+        message: "hello"
+    })
 })
 
 app.get("/hello", (req, res) => {
@@ -29,14 +22,13 @@ app.get("/hello", (req, res) => {
         message: "hello"
     })
 })
-// console.log("Express app is running")
-app.get("*", async (req, res) => {
-    res.send(await handler(req, "hello"))
-})
+
 app.post("*", async (req, res) => {
     res.send(await handler(req, "hello"))
 })
 
-// app.listen(4040, () => {
-//     console.log(`Server is running on http://localhost:${4040}`)
-// })
+const PORT = process.env.PORT || 4040;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${4040}`)
+})
