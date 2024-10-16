ae2a5db095433e1b8b6df32816fc4140ecbeb266
Anil Kumar Behera <ark845612@gmail.com>
2024-10-16 22:36:10 +0530

    test

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,18 @@
 import express from "express"
-import config from "./config";
+import path from "path"
 import { handler } from "./controller";
 
 export const app = express();
 
 app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.use((req, res, next) => {
     console.log(req.method, req.path);
     next();
 })
 
-const PORT = config.port;
+// const PORT = config.port;
 
 // app.get("/xyz", (req, res) => {
 //     console.log("xyz")
@@ -18,6 +20,9 @@ const PORT = config.port;
 //         message: "hellllodasj"
 //     })
 // })
+app.get("/home", async (req, res) => {
+    res.sendFile(path.join(__dirname, "public", 'index.html'))
+})
 
 app.get("/hello", (req, res) => {
     res.json({
@@ -32,6 +37,6 @@ app.post("*", async (req, res) => {
     res.send(await handler(req, "hello"))
 })
 
-app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`)
+app.listen(4040, () => {
+    console.log(`Server is running on http://localhost:${4040}`)
 })
