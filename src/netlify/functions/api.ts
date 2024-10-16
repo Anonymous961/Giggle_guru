00b7e4024595a5629f8b36ad1c75af41e7c9309d
Anil Kumar Behera <ark845612@gmail.com>
2024-10-16 23:05:59 +0530

    test2

--- src/netlify/functions/api.ts
+++ src/netlify/functions/api.ts
@@ -1,5 +1,10 @@
 import serverless from "serverless-http"
 import { app } from "../../index"
+import { Router } from "express";
 
+// const router = Router();
+// router.get("/hello", (req, res) => res.send("Hello World!"));
+
+// app.use("/", router);
 
 export const handler = serverless(app)
